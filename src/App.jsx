6b0db4e43a06632e66c4fff17e910c33d4c486c1
Tushar <tusharanshu18@gmail.com>
2025-09-21 15:46:26 +0530

    error changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar.jsx';
 import Header from './components/Header.jsx';
 import Headerimg from './components/headerimg.jsx';
 import Headerbtn from './components/Headerbtn.jsx';
-import Headercard from './components/Headercard.jsx';
+// import Headercard from './components/Headercard.jsx';
 import Topsellingbtn from './components/Topselling/Topsellingbtn.jsx';
 import Chooseus from './Whychooseus/choose.jsx';
 import Products from './Allproducts/products.jsx';
@@ -18,7 +18,7 @@ function App() {
       <Header/>
       <Headerimg/>
       <Headerbtn/>
-      <Headercard/>
+      {/* <Headercard/> */}
       <Topsellingbtn/>
       <Chooseus/>
       <Products/>
